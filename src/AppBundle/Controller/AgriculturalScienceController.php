2102dd1b45c767e33e47b628e5b9aa43bc799210
eyobiel <eyobielyebio@gmail.com>
2017-01-26 12:18:33 +0100

    Agricultural science page created with AgriculturalScienceController

---
+++ src/AppBundle/Controller/AgriculturalScienceController.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This AgriculturalScienceController is created for Agricultural Science page of the consultancy agency website. 
+ *
+ * @author Eyobiel
+ */
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AgriculturalScienceController extends Controller {
+
+    /**
+     * This method renders a response agriculturalScience.html.twig from the consultancy folder inside views
+     *
+     * @Route("/agricultural")
+     *
+     * @param $request Request
+     *
+     * @return Response
+     */
+
+    public function agriculturalAction(Request $request) {
+        $response = $this->render('consultancy/agriculturalScience.html.twig');
+
+        return $response;
+    }
+
+}
